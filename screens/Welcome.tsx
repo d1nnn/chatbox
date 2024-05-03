c5536275d0767d0636f786af7662de5b7597a7d1
Võ Nam <VoTuanNam>
2024-05-03 15:55:06 +0700

    Update splash screen

--- screens/Welcome.tsx
+++ screens/Welcome.tsx
@@ -1,32 +1,72 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, Image, ImageBackground } from "react-native";
 import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 import { NavigationProp } from "../props/Navigation";
+import * as Animatable from "react-native-animatable";
 
-export default function Welcome({ navigation }: NavigationProp): React.JSX.Element {
-
+export default function Welcome({
+  navigation,
+}: NavigationProp): React.JSX.Element {
   function navigateToLogin() {
     navigation?.navigate("Auth");
   }
 
   return (
-    <View style={styles.container}>
-      <Text>THIS IS WELCOME PAGE</Text>
-      <TouchableWithoutFeedback onPress={navigateToLogin}><Text style={styles.textStyle}>Start chatting</Text></TouchableWithoutFeedback>
-    </View>
-  )
+    <ImageBackground
+      source={require("../assets/imagess.jpg")}
+      style={styles.container}
+    >
+      <Animatable.Text
+        style={styles.wellcomeStyle}
+        animation="fadeInDownBig"
+        duration={2000}
+      >
+        WELCOME TO CHAT BOX
+      </Animatable.Text>
+      <Animatable.Image
+        source={require("../assets/group-chat.png")}
+        style={styles.splashImage}
+        animation="zoomIn"
+        duration={2000}
+      />
+
+      <TouchableWithoutFeedback onPress={navigateToLogin}>
+        <Animatable.Text
+          style={styles.textStyle}
+          animation="fadeInUpBig"
+          duration={2000}
+        >
+          Start Chatting
+        </Animatable.Text>
+      </TouchableWithoutFeedback>
+    </ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   textStyle: {
     padding: 15,
-    backgroundColor: 'orange',
-    color: 'black',
-    borderRadius: 10,
-  }
-})
+    backgroundColor: "orange",
+    color: "black",
+    borderRadius: 20,
+    marginTop: 20,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  wellcomeStyle: {
+    paddingBottom: 70,
+    fontWeight: "bold",
+    fontSize: 30,
+    color: "orange",
+  },
+  splashImage: {
+    width: 150,
+    height: 150,
+    marginBottom: 10,
+  },
+});
